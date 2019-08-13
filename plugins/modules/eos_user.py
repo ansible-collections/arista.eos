4da848894a8e9a2ab09586102771a7aade7f0f37
Paul Belanger <pabelanger@redhat.com>
2019-08-13 12:50:26 -0400

    Add zuul jobs for linting
    This includes code changes needed for linting.
    
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- plugins/modules/eos_user.py
+++ plugins/modules/eos_user.py
@@ -181,7 +181,6 @@ def validate_privilege(value, module):
 
 def map_obj_to_commands(updates, module):
     commands = list()
-    state = module.params["state"]
     update_password = module.params["update_password"]
 
     for update in updates:
