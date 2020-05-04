42a73feabc716f063829639bc86e1e154c941b13
GomathiselviS <gomathiselvi@gmail.com>
2020-05-04 18:10:37 -0400

    Added new states eos_lacp
    Signed-off-by: GomathiselviS <gomathiselvi@gmail.com>

--- plugins/module_utils/network/eos/argspec/lacp/lacp.py
+++ plugins/module_utils/network/eos/argspec/lacp/lacp.py
@@ -47,8 +47,17 @@ class LacpArgs(object):
             },
             "type": "dict",
         },
+        "running_config": {"type": "str"},
         "state": {
-            "choices": ["merged", "replaced", "deleted"],
+            "choices": [
+                "merged",
+                "replaced",
+                "overridden",
+                "deleted",
+                "gathered",
+                "rendered",
+                "parsed",
+            ],
             "default": "merged",
             "type": "str",
         },
