ab328955fdce0d462a9d8faa445bb71e57b42628
GomathiselviS <gomathiselvi@gmail.com>
2020-05-11 10:35:08 -0400

    removed overridden from empty_cpnfig test

--- plugins/module_utils/network/eos/config/lacp/lacp.py
+++ plugins/module_utils/network/eos/config/lacp/lacp.py
@@ -120,10 +120,7 @@ class Lacp(ConfigBase):
                   to the desired configuration
         """
         state = self._module.params["state"]
-        if (
-            state in ("merged", "replaced", "rendered")
-            and not want
-        ):
+        if state in ("merged", "replaced", "rendered") and not want:
             self._module.fail_json(
                 msg="value of config parameter must not be empty for state {0}".format(
                     state
