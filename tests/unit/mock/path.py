770672fdc4112dff681f47822387f6e01f957559
Nathaniel Case <Qalthos@gmail.com>
2020-06-23 23:31:08 -0400

    Clean up boilerplate ignores (#64)
    Clean up boilerplate ignores
    
    Reviewed-by: https://github.com/apps/ansible-zuul

--- tests/unit/mock/path.py
+++ tests/unit/mock/path.py
@@ -1,3 +1,6 @@
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
 from ansible_collections.arista.eos.tests.unit.compat.mock import MagicMock
 from ansible.utils.path import unfrackpath
 
