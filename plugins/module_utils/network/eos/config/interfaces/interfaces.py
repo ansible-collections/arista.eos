01a7b9430f75b3b49998e74d28ce0c52324963f1
GomathiselviS <gomathiselvi@gmail.com>
2020-09-28 16:03:03 -0400

    handled management int

--- plugins/module_utils/network/eos/config/interfaces/interfaces.py
+++ plugins/module_utils/network/eos/config/interfaces/interfaces.py
@@ -14,6 +14,8 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
+import re
+
 from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.cfg.base import (
     ConfigBase,
 )
@@ -254,12 +256,13 @@ def generate_commands(interface, to_set, to_remove):
             # duplex is handled with speed
             continue
         elif key == "mode":
-            if value == "layer3":
-                # switching from default (layer2) mode to layer3
-                commands.append("no switchport")
-            else:
-                # setting to default (layer 2) mode
-                commands.append("switchport")
+            if not re.search(r'(M|m)anagement.*', interface):
+                if value == "layer3":
+                    # switching from default (layer2) mode to layer3
+                    commands.append("no switchport")
+                else:
+                    # setting to default (layer 2) mode
+                    commands.append("switchport")
         else:
             commands.append("{0} {1}".format(key, value))
 
@@ -275,7 +278,8 @@ def generate_commands(interface, to_set, to_remove):
             # duplex is handled with speed
             continue
         elif key == "mode":
-            commands.append("switchport")
+            if not re.search(r'(M|m)anagement.*', interface):
+                commands.append("switchport")
         else:
             commands.append("no {0}".format(key))
 
