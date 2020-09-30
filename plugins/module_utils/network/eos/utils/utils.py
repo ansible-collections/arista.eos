7b9acfd5fecc912c7bc1a6fe95dcfae60080221d
GomathiselviS <gomathiselvi@gmail.com>
2020-09-30 12:39:37 -0400

    eos_l2_interfaces : fix idempotent issue when allowed_vlans is set (#119)
    eos_l2_interfaces : fix idempotent issue when allowed_vlans is set
    
    Reviewed-by: https://github.com/apps/ansible-zuul

--- plugins/module_utils/network/eos/utils/utils.py
+++ plugins/module_utils/network/eos/utils/utils.py
@@ -53,3 +53,33 @@ def normalize_interface(name):
         proper_interface = name
 
     return proper_interface
+
+
+def vlan_range_to_list(vlans):
+    result = []
+    if vlans:
+        if isinstance(vlans, str):
+            vlans = vlans.split(",")
+        for part in vlans:
+            if part == "none":
+                break
+            if "-" in part:
+                a, b = part.split("-")
+                a, b = int(a), int(b)
+                result.extend(range(a, b + 1))
+            else:
+                a = int(part)
+                result.append(a)
+        return numerical_sort(result)
+    return result
+
+
+def numerical_sort(string_int_list):
+    """Sorts list of integers that are digits in numerical order.
+    """
+    as_int_list = []
+
+    for vlan in string_int_list:
+        as_int_list.append(int(vlan))
+    as_int_list.sort()
+    return list(set(as_int_list))
