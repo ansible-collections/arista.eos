f6d376f73293f51a98151ba6d5f1145e3b3678dd
Paul Belanger <pabelanger@redhat.com>
2021-01-14 12:42:24 -0500

    Updates needed to start using execution environments
    This change starts the process of adding support for execution
    environments for running sanity / unit tests.
    
    Depends-On: https://github.com/ansible/ansible-zuul-jobs/pull/742
    Depends-On: https://github.com/ansible/network-ee/pull/27
    Depends-On: https://github.com/ansible-collections/arista.eos/pull/154
    Depends-On: https://github.com/ansible/ansible-zuul-jobs/pull/745
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- plugins/modules/eos_vrf.py
+++ plugins/modules/eos_vrf.py
@@ -139,8 +139,10 @@ EXAMPLES = """
 - name: Create aggregate of VRFs with purge
   arista.eos.eos_vrf:
     aggregate:
-    - {name: test4, rd: 1:204}
-    - {name: test5, rd: 1:205}
+    - name: test4
+      rd: 1:204
+    - name: test5
+      rd: 1:205
     state: present
     purge: yes
 
