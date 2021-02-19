938220ba74caaf89c80f2c9b0b46e506e17a540c
GomathiselviS <gomathiselvi@gmail.com>
2021-02-19 12:20:36 -0500

    Changed import path for resource_module class in unit tests (#171)
    Changed import path for resource_module class in unit tests
    
    Reviewed-by: https://github.com/apps/ansible-zuul

--- tests/unit/modules/network/eos/test_eos_bgp_address_family.py
+++ tests/unit/modules/network/eos/test_eos_bgp_address_family.py
@@ -24,7 +24,7 @@ class TestEosBgpafModule(TestEosModule):
         super(TestEosBgpafModule, self).setUp()
 
         self.mock_get_resource_connection_config = patch(
-            "ansible_collections.ansible.netcommon.plugins.module_utils.network.common.resource_module.get_resource_connection"
+            "ansible_collections.ansible.netcommon.plugins.module_utils.network.common.rm_base.resource_module_base.get_resource_connection"
         )
         self.get_resource_connection_config = (
             self.mock_get_resource_connection_config.start()
