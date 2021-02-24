aee19f37a0c2a831473b1bbffa039646441a4557
Ganesh Nalawade <ganesh634@gmail.com>
2021-02-24 21:05:07 +0530

    Move warning message for eos_config module (#175)
    Move warning message for eos_config module
    
    Reviewed-by: https://github.com/apps/ansible-zuul

--- plugins/modules/eos_config.py
+++ plugins/modules/eos_config.py
@@ -446,14 +446,6 @@ def main():
             result["__backup__"] = contents
 
     if any((module.params["src"], module.params["lines"])):
-        msg = (
-            "To ensure idempotency and correct diff the input configuration lines should be"
-            " similar to how they appear if present in the running configuration on device"
-        )
-        if module.params["src"]:
-            msg += " including the indentation"
-        warnings.append(msg)
-
         match = module.params["match"]
         replace = module.params["replace"]
         path = module.params["parents"]
@@ -585,6 +577,21 @@ def main():
                         "diff": {"before": str(before), "after": str(after)},
                     }
                 )
+
+    if result.get("changed") and any(
+        (module.params["src"], module.params["lines"])
+    ):
+        msg = (
+            "To ensure idempotency and correct diff the input configuration lines should be"
+            " similar to how they appear if present in"
+            " the running configuration on device"
+        )
+        if module.params["src"]:
+            msg += " including the indentation"
+        if "warnings" in result:
+            result["warnings"].append(msg)
+        else:
+            result["warnings"] = msg
 
     module.exit_json(**result)
 
