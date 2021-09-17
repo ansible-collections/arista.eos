b713a079653f7cf810f01db7454809c34f1ab7fa
Nathaniel Case <this.is@nathanielca.se>
2021-09-17 11:25:46 -0400

    Document expected suboptions to eos_command's command argument

--- plugins/modules/eos_command.py
+++ plugins/modules/eos_command.py
@@ -47,6 +47,57 @@ options:
     required: true
     type: list
     elements: raw
+    suboptions:
+      command:
+        description:
+        - The command to send to the remote network device.  The resulting output from
+          the command is returned, unless I(sendonly) is set.
+        required: true
+        type: str
+      output:
+        description:
+        - How the remote device should format the command response data.
+        type: str
+        choices: ["text", "json"]
+      prompt:
+        description:
+        - A single regex pattern or a sequence of patterns to evaluate the expected prompt
+          from I(command).
+        required: false
+        type: list
+        elements: str
+      answer:
+        description:
+        - The answer to reply with if I(prompt) is matched. The value can be a single
+          answer or a list of answer for multiple prompts. In case the command execution
+          results in multiple prompts the sequence of the prompt and excepted answer should
+          be in same order.
+        required: false
+        type: list
+        elements: str
+      sendonly:
+        description:
+        - The boolean value, that when set to true will send I(command) to the device
+          but not wait for a result.
+        type: bool
+        default: false
+        required: false
+      newline:
+        description:
+        - The boolean value, that when set to false will send I(answer) to the device
+          without a trailing newline.
+        type: bool
+        default: true
+        required: false
+      check_all:
+        description:
+        - By default if any one of the prompts mentioned in C(prompt) option is matched
+          it won't check for other prompts. This boolean flag, that when set to I(True)
+          will check for all the prompts mentioned in C(prompt) option in the given order.
+          If the option is set to I(True) all the prompts should be received from remote
+          host if not it will result in timeout.
+        type: bool
+        default: false
   wait_for:
     description:
     - Specifies what to evaluate from the output of the command and what conditionals
