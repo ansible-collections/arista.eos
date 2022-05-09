e45157c969fcbd1c9b113cff808bbc42d3e22a6b
GomathiselviS <gomathiselvi@gmail.com>
2022-05-09 12:25:30 -0400

    Fix logic to add new aces with replaced and overridden state
    Signed-off-by: GomathiselviS <gomathiselvi@gmail.com>

--- tests/unit/modules/network/eos/test_eos_acls.py
+++ tests/unit/modules/network/eos/test_eos_acls.py
@@ -185,6 +185,21 @@ class TestEosAclsModule(TestEosModule):
                                         log="true",
                                     ),
                                 ],
+                            ),
+                            dict(
+                                name="test3",
+                                aces=[
+                                    dict(
+                                        sequence="50",
+                                        grant="permit",
+                                        protocol="ospf",
+                                        source=dict(
+                                            subnet_address="70.2.0.0/8"
+                                        ),
+                                        destination=dict(any="true"),
+                                        log="true",
+                                    ),
+                                ],
                             )
                         ],
                     )
@@ -198,6 +213,8 @@ class TestEosAclsModule(TestEosModule):
             "no 45",
             "10 permit ospf 30.2.0.0/8 any log",
             "20 permit ospf 40.2.0.0/8 any log",
+            "ip access-list test3",
+            "50 permit ospf 70.2.0.0/8 any log"
         ]
         self.execute_module(changed=True, commands=commands)
 
@@ -259,6 +276,21 @@ class TestEosAclsModule(TestEosModule):
                                         log="true",
                                     )
                                 ],
+                            ),
+                            dict(
+                                name="test3",
+                                aces=[
+                                    dict(
+                                        sequence="50",
+                                        grant="permit",
+                                        protocol="ospf",
+                                        source=dict(
+                                            subnet_address="70.2.0.0/8"
+                                        ),
+                                        destination=dict(any="true"),
+                                        log="true",
+                                    ),
+                                ],
                             )
                         ],
                     )
@@ -271,6 +303,8 @@ class TestEosAclsModule(TestEosModule):
             "no 35",
             "no 45",
             "10 permit ospf 30.2.0.0/8 any log",
+            "ip access-list test3",
+            "50 permit ospf 70.2.0.0/8 any log",
         ]
         self.execute_module(changed=True, commands=commands)
 
