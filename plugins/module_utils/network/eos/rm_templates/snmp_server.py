90c6c69b4e0155c947e13c68d8fec2ddd365f6df
Martin Beckert <beckert@strato.de>
2023-12-06 12:14:55 +0100

    Add 'auth' to '_tmplt_snmp_server_users_localized()'

--- plugins/module_utils/network/eos/rm_templates/snmp_server.py
+++ plugins/module_utils/network/eos/rm_templates/snmp_server.py
@@ -351,7 +351,7 @@ def _tmplt_snmp_server_users_localized(config_data):
     if el.get("localized"):
         command += " localized " + el["localized"]["engineid"]
         el = el["localized"]
-        command += " " + el["algorithm"] + " " + el["auth_passphrase"]
+        command += " auth " + el["algorithm"] + " " + el["auth_passphrase"]
         if el.get("encryption"):
             command += " priv " + el["encryption"] + " " + el["priv_passphrase"]
     return command
