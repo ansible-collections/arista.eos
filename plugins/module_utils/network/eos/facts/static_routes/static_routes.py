2bfe2069469b749114cb597361b4591fc88e2f2d
rohitthakur2590 <rohitthakur2590@outlook.com>
2024-06-10 21:42:36 +0530

    update test

--- plugins/module_utils/network/eos/facts/static_routes/static_routes.py
+++ plugins/module_utils/network/eos/facts/static_routes/static_routes.py
@@ -54,21 +54,15 @@ class Static_routesFacts(object):
         :rtype: dictionary
         :returns: facts
         """
-        import q
         if not data:
             data = self.get_device_data(connection)
-            q(data)
 
-        # split the config into instances of the resource
-        resource_delim = "ip.* route|ipv6.* route"
-        find_pattern = r"(?:^|\n)%s.*?(?=(?:^|\n)%s|$)" % (
-            resource_delim,
-            resource_delim,
-        )
-        resources = [p.strip() for p in re.findall(find_pattern, data)]
+        resource_delim = r"(?:ip route|ipv6 route)"
+        find_pattern = r"(?:^|\n)(%s [^\n]*)(?=(?:^|\n)(?:%s|router ospf|$))" % (resource_delim, resource_delim)
+        resources = [p.strip() for p in re.findall(find_pattern, data, re.DOTALL)]
         resources_without_vrf = []
         resource_vrf = {}
-        q(resources)
+        
         for resource in resources:
             if resource and "vrf" not in resource:
                 resources_without_vrf.append(resource)
