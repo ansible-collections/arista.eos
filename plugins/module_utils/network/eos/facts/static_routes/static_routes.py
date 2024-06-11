73563a2c22c7db83d1ba39feaa785d4969ac030e
Rohit Thakur <rohitthakur2590@outlook.com>
2024-06-11 17:53:16 +0530

    Fix IPv6 static routes facts (#543)
    * Fix IPv6 static routes facts
    
    Signed-off-by: rohitthakur2590 <rohitthakur2590@outlook.com>
    
    * update test
    
    * Update tests
    
    Signed-off-by: rohitthakur2590 <rohitthakur2590@outlook.com>
    
    * trivial
    
    Signed-off-by: rohitthakur2590 <rohitthakur2590@outlook.com>
    
    ---------
    
    Signed-off-by: rohitthakur2590 <rohitthakur2590@outlook.com>

--- plugins/module_utils/network/eos/facts/static_routes/static_routes.py
+++ plugins/module_utils/network/eos/facts/static_routes/static_routes.py
@@ -44,7 +44,7 @@ class Static_routesFacts(object):
         self.generated_spec = utils.generate_dict(facts_argument_spec)
 
     def get_device_data(self, connection):
-        return connection.get("show running-config | grep route")
+        return connection.get("show running-config | include ^ip route|^ipv6 route")
 
     def populate_facts(self, connection, ansible_facts, data=None):
         """Populate the facts for static_routes
